1495e38db0793d01825c27250e0742054dbe9a98
darcy-macos <darcy-macos@gmail.com>
2024-10-11 17:21:54 +0800

    add 正常打包

---
+++ packages/vite-project/index.ts
@@ -0,0 +1,4 @@
+export default function show(){
+    console.log('vite-project ++++++++++++++++++++++');
+    
+}
--- packages/vite-project/vite.config.ts
+++ packages/vite-project/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build:{
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'ala-vite-project',
+      fileName: (format) => `ala-lib-vite-project.${format}.js`
+    },
+  }
 })
