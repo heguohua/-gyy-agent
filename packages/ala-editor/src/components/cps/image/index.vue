f921ae8b4193b3f7550afaea52c84d4dd65fdc4d
darcy.zhang <tech.darcy.zhang@outlook.com>
2024-10-21 21:06:26 +0800

    feat(editor页面): 完成editor页面config区域配置参数并在render区域渲染数据流调试

--- packages/ala-editor/src/components/cps/image/index.vue
+++ packages/ala-editor/src/components/cps/image/index.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-10-11 09:06:05
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2024-10-18 22:27:19
+ * @LastEditTime: 2024-10-21 21:05:10
  * @FilePath: /low-coding/packages/ala-editor/src/components/cps/image/index.vue
  * @Description: 
  * 
@@ -19,7 +19,7 @@
     </div>
   </div>
 </template>
-<script lang="ts">
+<script setup lang="ts">
 
 import { logger } from '@/utils/logger';
 import { defineComponent, ref } from 'vue'
@@ -27,41 +27,29 @@ import { createNamespace } from '@/components/cps/utils/createNamespace';
 import { blockProps } from './props';
 
 const { name, bem } = createNamespace("image")
-
-
-export default defineComponent({
-  name,
-  props: blockProps,
-  setup(props) {
-    logger.warn("AlaImage组件被渲染");
-
-    const classes = computed(() => [bem()])
-
-    const { data, viewport } = toRefs(props)
-    const display = computed(() => data.value?.display?.[viewport.value] || '');
-    const src = computed(() => data.value?.src?.[viewport.value] || '')
-    const link = computed(() => data.value?.link?.[viewport.value] || '')
-    const width = computed(() => data.value?.width?.[viewport.value] || '')
-    const height = computed(() => data.value?.height?.[viewport.value] || '')
-
-    const styles = computed(() => {
-      return { width: width.value, height: height.value }
-    })
-
-
-    return {
-      classes,
-      bem,
-      styles,
-      display,
-      src,
-      link,
-      width,
-      height
-    }
-
-  }
+defineOptions({
+  name: "ala-image"
 })
+
+const props = defineProps(blockProps)
+
+
+logger.warn("AlaImage组件被渲染");
+
+const classes = computed(() => [bem()])
+
+const { data, viewport } = toRefs(props)
+const display = computed(() => data.value?.display?.[viewport.value] || '');
+const src = computed(() => data.value?.src?.[viewport.value] || '')
+const link = computed(() => data.value?.link?.[viewport.value] || '')
+const width = computed(() => data.value?.width?.[viewport.value] || '')
+const height = computed(() => data.value?.height?.[viewport.value] || '')
+
+const styles = computed(() => {
+  return { width: width.value, height: height.value }
+})
+
+
 
 </script>
 
