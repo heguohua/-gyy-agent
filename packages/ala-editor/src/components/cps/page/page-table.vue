4295a67d0cba5565196d9ce1011c1ba4de7b1cef
darcy.zhang <tech.darcy.zhang@outlook.com>
2024-11-15 14:46:11 +0800

    feat(添加分页列表公共页面): 添加分页列表公共页

---
+++ packages/ala-editor/src/components/cps/page/page-table.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
+ * @Date: 2024-11-15 14:44:47
+ * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
+ * @LastEditTime: 2024-11-15 14:44:51
+ * @FilePath: /1-low-coding/packages/ala-editor/src/components/cps/page/page-table.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by 【 tech.darcy.zhang@outlook.com 】, All Rights Reserved. 
+-->
+<template>
+    
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// State
+
+
+// Methods
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
