1c13c0621d1aac9095aea81d46c57c398b44ffee
darcy.zhang <tech.darcy.zhang@outlook.com>
2024-11-21 21:15:25 +0800

    refactor(删除冗余代码): 删除冗余代码

--- packages/ala-editor/src/components/cps/select/ala-select.vue
+++ packages/ala-editor/src/components/cps/select/ala-select.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-11-11 21:55:35
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2024-11-15 11:29:25
+ * @LastEditTime: 2024-11-21 21:15:04
  * @FilePath: /1-low-coding/packages/ala-editor/src/components/cps/select/ala-select.vue
  * @Description: 
  * 
@@ -88,9 +88,5 @@ const clasz = computed(() => {
 </script>
 
 <style scoped lang="scss">
-.ala-select-wrapper {
-  .ala-select-group {
-    .el-select-item {}
-  }
-}
+
 </style>
