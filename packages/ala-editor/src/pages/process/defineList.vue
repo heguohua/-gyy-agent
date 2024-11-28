0dde952cfb3bff24692e468ab538d402d9fd40b7
darcy.zhang <tech.darcy.zhang@outlook.com>
2024-11-28 20:03:26 +0800

    feat(添加 流程定义 模块): 添加 流程定义 模块

--- packages/ala-editor/src/pages/process/defineList.vue
+++ packages/ala-editor/src/pages/process/defineList.vue
@@ -1,27 +1,122 @@
 <!--
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
- * @Date: 2024-11-28 15:57:05
+ * @Date: 2024-11-11 11:20:08
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2024-11-28 17:59:22
+ * @LastEditTime: 2024-11-28 20:01:34
  * @FilePath: /1-low-coding/packages/ala-editor/src/pages/process/defineList.vue
  * @Description: 
  * 
  * Copyright (c) 2024 by 【 tech.darcy.zhang@outlook.com 】, All Rights Reserved. 
 -->
 <template>
-    流程定义列表 页面
+    <!-- 查询条件 -->
+    <SearchPanel :baseFields="baseFields" :advancedFields="advancedFields" :params="params" @refresh="refresh"
+        @showAdd="showAdd({ id: null, pid: 0 })" labelWidth="180px" />
+
+    <!-- 分页列表 -->
+    <PageNestingTable ref="pageRef" :url="url" :deleteUrl="deleteUrl" :columns="columns" :params="params"
+        :showSelectCheckbox="false" @add="showAdd" @edit="showEdit" :tipTitle="$t('pop.warm_title')" />
+
+    <!-- 新增、编辑 -->
+    <!-- <MenuAdd @refresh="refresh" v-model="showAddForm" :baseInfo="baseInfo" /> -->
+
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts" setup>
+import { ref } from 'vue';
+import MenuAdd from '@/pages/menu/menuAdd.vue';
+import { useRoute } from 'vue-router';
+import PageNestingTable from '@/components/cps/page/page-nesting-table.vue';
+import { logger } from '@/utils/logger';
+import { alaBuildInput } from '@/config/alaBuilders';
+import u from '@/utils/u';
+import { id } from 'element-plus/es/locale';
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
 
-// State
+// ############## 初始化基本数据，该部分代码不用修改 start ######################################
+// 1、获取当前模块名
+const route = useRoute();
+const moduleName = computed(() => {
+    const code = route.meta.menuCode as string;
+    return t(code)
+})
+// 2、定义当前编辑对象id
+const baseInfo = reactive({
+    moduleName,
+    id: null,
+    selectedList: Array<{ id: string }>,
+    item: {}
+})
+provide('baseInfo', baseInfo);
+
+// ############## 初始化基本数据，该部分代码不用修改 end ######################################
+
+// ############## 分页列表通用方法，该部分代码不用修改 start ######################################
+
+const showAddForm = ref(false)
+const showAdd = (item: { [key: string]: any }) => {
+    u.clear(baseInfo.item)
+    u.merged(baseInfo, { item: { id: null, pid: item.id } })
+    logger.info(`【新增】方法接收到参数【 item 】`, item);
+    logger.info(`当前模块【 baseInfo 】对象参数为`, baseInfo);
+    showAddForm.value = true
+}
+
+const showEdit = (item: { [key: string]: any }) => {
+    showAddForm.value = true
+    u.merged(baseInfo, { item })
+    logger.info(`【编辑】方法接收到参数 item `, item);
+    logger.info(`当前模块【 baseInfo 】对象参数为`, baseInfo);
+    showAddForm.value = true
+}
+
+// 查询条件
+const params = reactive({})
+
+const pageRef = ref<InstanceType<typeof PageNestingTable> | null>(null)
+const refresh = () => {
+    if (pageRef.value) {
+        pageRef.value.refresh(params)
+    }
+}
+
+// ############## 分页列表通用方法，该部分代码不用修改 end ######################################
 
 
-// Methods
+// ############## 分页列表自定义方法，该部分代码需要按需定制 start ######################################
+
+const url = "/p/define/page"
+const deleteUrl = "/u/define/delete"
+
+// 分页列表中列属性配置
+const columns = computed(() => {
+    return [
+        { prop: 'displayName', label: '名称' },
+        { prop: 'name', label: '唯一编码' },
+        { prop: 'type', label: '流程分类' },
+        { prop: 'version', label: '版本号' },
+        { prop: 'state', label: '状态' },
+    ]
+})
+
+
+// 基础查询条件
+const baseFields = computed(() => {
+    return [
+        alaBuildInput("displayName", '名称'),
+    ]
+})
+
+
+// 高级查询条件
+const advancedFields = []
+
+
+// ############## 分页列表自定义方法，该部分代码需要按需定制 end ######################################
+
+
 
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style lang="scss" scoped></style>
