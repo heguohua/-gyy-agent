ee58036acf864fef50ec48bcb45a4c340707f1be
darcy.zhang <tech.darcy.zhang@outlook.com>
2024-12-07 11:31:26 +0800

    feat(初步开发弹窗选择组件): 初步开发弹窗选择组件

--- packages/ala-editor/src/components/cps/select-table/ala-select-table.vue
+++ packages/ala-editor/src/components/cps/select-table/ala-select-table.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-11-11 21:55:35
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2024-12-07 08:44:40
+ * @LastEditTime: 2024-12-07 11:29:42
  * @FilePath: /1-low-coding/packages/ala-editor/src/components/cps/select-table/ala-select-table.vue
  * @Description: 
  * 
@@ -12,12 +12,41 @@
   <div class="ala-select-table-wrapper">
     <!-- <span :class="clasz">{{ label }}</span>  -->
     <el-form-item :label="label" :label-position="position" :prop="fieldName">
-      <el-select @change="handleChange" :model-value="model" class="ala-select-group" :style="styles" :id="fieldName">
+      <!-- <el-select @change="handleChange" :model-value="model" class="ala-select-group" :style="styles" :id="fieldName">
         <div class="el-select-item" v-for="(item, index) in items" :key="item.value">
           <el-option :key="item.value" :label="item.name" :value="item.value" />
         </div>
-      </el-select>
+      </el-select> -->
+
+      <!-- <el-input :disabled="true" suffix-icon="el-icon-more" @click.native="openDialog" :model-value="model" :id="fieldName">
+      </el-input> -->
+      <div class="ala-select-customer">
+        <input type="hidden" :model-value="model" :id="fieldName">
+        eee
+      </div>
+      <div class="ala-select-customer-icon">
+        <v-icon class="icon" icon="add" @click="openDialog" />
+      </div>
     </el-form-item>
+
+    <el-dialog v-model="dialogShow" width="80%" :append-to-body="true">
+      <div class="dialog-content">
+        <div class="left-panel">
+          <!-- 分页列表 -->
+          <PageTableSelect ref="pageRef" :url="url" :columns="columns" :params="params" :showSelectCheckbox="true"
+            :tipTitle="$t('pop.warm_title')" />
+
+        </div>
+        <div class="right-panel">
+          <!-- 勾选的数据列表 -->
+          <el-table :data="selectedData">
+          </el-table>
+          <el-button @click="removeSelected">删除</el-button>
+        </div>
+      </div>
+
+    </el-dialog>
+
   </div>
 </template>
 
@@ -25,7 +54,14 @@
 import { logger } from '@/utils/logger';
 import { alaPost } from '@/utils/req';
 import u from '@/utils/u';
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
+import PageTableSelect from '@/components/cps/page/page-table-select.vue';
 
+interface Column {
+  prop: string;
+  label: string;
+}
 
 interface ItemProperty {
   propertyName: string,
@@ -68,9 +104,9 @@ const props = defineProps({
   }
 })
 
-interface item{
-  name:string,
-  value:string,
+interface item {
+  name: string,
+  value: string,
 }
 
 const items = ref<Array<item>>([])
@@ -86,6 +122,13 @@ const handleChange = (value: any) => {
   model.value = value
 }
 
+// 分页列表中列属性配置
+const columns = computed(() => {
+  return [
+    { prop: 'name', label: t('module.menu.name') },
+    { prop: 'delFlag', label: t('common.enable') },
+  ]
+})
 
 // Methods
 const url = props.url
@@ -95,17 +138,42 @@ logger.info(`从 api 加载下拉组件数据，url【 ${url} 】，查询参数
 alaPost(u.url(url), params, false, '').then((data: any) => {
   const response = data;
   if (response.data) {
-    const item_s:Array<item> = []
+    const item_s: Array<item> = []
     response.data.forEach((item: any) => {
       const name = item[props.itemProperty.propertyName]
       const value = item[props.itemProperty.valueName]
       item_s.push({ name, value })
     })
-    u.merged(items.value,item_s)
+    u.merged(items.value, item_s)
   }
 
 });
 
+const dialogShow = ref(false)
+const openDialog = () => {
+  console.log('111:', 111);
+
+  dialogShow.value = true;
+}
+
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.ala-select-table-wrapper {
+  .ala-select-customer {
+    input {}
+  }
+
+  .ala-select-customer-icon {
+    .icon {}
+  }
+
+  .dialog-content {
+    z-index: 10000;
+
+    .left-panel {}
+
+    .right-panel {}
+  }
+}
+</style>
--- packages/ala-editor/src/pages/menu/index.vue
+++ packages/ala-editor/src/pages/menu/index.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-11-11 11:20:08
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2024-12-06 11:36:57
+ * @LastEditTime: 2024-12-07 10:45:04
  * @FilePath: /1-low-coding/packages/ala-editor/src/pages/menu/index.vue
  * @Description: 
  * 
@@ -145,7 +145,7 @@ const advancedFields = [
     { componentName: 'AlaSlider', label: '取值范围', placeholder: '请指定取值范围', fieldName: 'slider2', other: { min: 2, max: 10, step: 1, } },
     { componentName: 'AlaRating', label: '评分', placeholder: '请指定评分', fieldName: 'rating2', other: { max: 8, allowHalf: true } },
     alaBuildSelectApi("select_api", "api下拉选", "/u/menu/list", { propertyName: 'name', valueName: 'id' }, undefined, { value: '1' }, "请选择"),
-    alaBuildSelectTable("select_table", "table下拉选", "/u/menu/list", { propertyName: 'name', valueName: 'id' }, undefined, { value: '1' }, "请选择"),
+    alaBuildSelectTable("select_table", "table下拉选", "/u/menu/page", { propertyName: 'name', valueName: 'id' }, undefined, { value: '1' }, "请选择"),
     alaBuildSelectDict("select_dict", "字典下拉选", {}, { propertyName: 'dictLabel', valueName: 'id', }, undefined, "请选择"),
 ]
 
