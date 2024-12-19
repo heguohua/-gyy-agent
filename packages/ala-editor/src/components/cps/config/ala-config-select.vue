1669df62c2d01f4c5e6b64d74c27a55232345270
darcy.zhang <tech.darcy.zhang@outlook.com>
2024-12-19 10:21:51 +0800

    feat(editor-config区域select组件添加清空参数配置项): editor-config区域select组件添加清空参数配置项

--- packages/ala-editor/src/components/cps/config/ala-config-select.vue
+++ packages/ala-editor/src/components/cps/config/ala-config-select.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-10-20 14:50:35
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2024-12-19 10:16:40
+ * @LastEditTime: 2024-12-19 10:20:40
  * @FilePath: /1-low-coding/packages/ala-editor/src/components/cps/config/ala-config-select.vue
  * @Description: 
  * 
