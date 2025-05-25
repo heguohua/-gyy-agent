1ffe2c30be242e530308e97950e89ecca642eb04
darcy.zhang <tech.darcy.zhang@outlook.com>
2025-05-25 17:55:44 +0800

    feat(集成d3矿建): 初步集成d3框架

---
+++ packages/ala-editor/src/pages/iot/point/index.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
+ * @Date: 2025-05-25 17:11:18
+ * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
+ * @LastEditTime: 2025-05-25 17:45:54
+ * @FilePath: /1-low-coding/packages/ala-editor/src/pages/iot/point/index.vue
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by 【 tech.darcy.zhang@outlook.com 】, All Rights Reserved. 
+-->
+<template>
+    <d_d3基础示例/>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import d_d3基础示例 from './d_d3基础示例.vue'
+
+// Methods
+
+</script>
+
+<style scoped lang="scss"></style>
--- packages/ala-editor/src/utils/menuRegister.ts
+++ packages/ala-editor/src/utils/menuRegister.ts
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-11-10 12:57:08
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2025-05-04 21:36:50
+ * @LastEditTime: 2025-05-25 17:10:52
  * @FilePath: /1-low-coding/packages/ala-editor/src/utils/menuRegister.ts
  * @Description: 
  * 
@@ -191,6 +191,11 @@ export const allMenuComponents: MenuComponent = {
     "/device/page": {
         name: "devicePage",
         component: "../pages/iot/device/index.vue",
+        requiresAuth: true,
+    },
+    "/point/page": {
+        name: "pointPage",
+        component: "../pages/iot/point/index.vue",
         requiresAuth: true,
     },
 
