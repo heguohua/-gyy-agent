7c034f92105a020ae70d96b17fce34badb102b73
darcy.zhang <tech.darcy.zhang@outlook.com>
2025-05-31 15:54:16 +0800

    feat(完成 拖拽 参考线绘制): 完成 拖拽 参考线绘制

--- packages/ala-editor/src/pages/drag/DropCanvas.vue
+++ packages/ala-editor/src/pages/drag/DropCanvas.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2025-05-30 21:51:44
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2025-05-31 11:25:34
+ * @LastEditTime: 2025-05-31 15:52:36
  * @FilePath: /1-low-coding/packages/ala-editor/src/pages/drag/DropCanvas.vue
  * @Description: 
  * 
@@ -10,13 +10,11 @@
 -->
 <template>
     <div class="drop-canvas" @dragover.prevent @drop="onDrop" ref="canvasRef">
-        <div v-for="(item, index) in droppedComponents" :key="item.id" class="dropped-item"
-            :style="{ top: item.y + 'px', left: item.x + 'px' }" @mousedown="startDrag">
+        <div v-for="(item, index) in droppedComponents" :key="item.id" :id="item.id" class="dropped-item"
+            :style="{ top: item.y + 'px', left: item.x + 'px', width: item.width + 'px', height: item.height + 'px', }"
+            @mousedown="startDrag" @click="onClick">
             {{ item.type }}
-
-
         </div>
-
 
         <!-- 对齐参考线 -->
         <div v-for="line in guidelines" :key="line.id" :class="['guide-line', line.direction]" :style="lineStyle(line)">
@@ -25,6 +23,7 @@
 </template>
 
 <script lang="ts" setup>
+import u from '@/utils/u'
 import { reactive } from 'vue'
 
 interface DroppedItem {
@@ -39,6 +38,19 @@ const canvasRef = ref<HTMLElement | null>(null)
 
 const droppedComponents = reactive<DroppedItem[]>([])
 
+const onClick = (e: MouseEvent) => {
+    const targetElement = e.target as HTMLElement;
+    const idValue = targetElement.id;
+    const item = droppedComponents.find((i) => i.id === idValue)!
+    console.log('当前图形 x :', item.x);
+    console.log('当前图形 y :', item.y);
+    console.log('当前图形 宽度 :', item.width);
+    console.log('当前图形 高度 :', item.height);
+
+
+}
+
+
 const onDrop = (e: DragEvent) => {
 
     const type = e.dataTransfer?.getData('component-type')
@@ -52,8 +64,8 @@ const onDrop = (e: DragEvent) => {
         type,
         x: e.offsetX,
         y: e.offsetY,
-        width: 100,
-        height: 150,
+        width: 200 * (Math.random() + 0.5),
+        height: 100 * (Math.random() + 0.5),
     })
 }
 
@@ -79,14 +91,22 @@ const onDrag = (e: MouseEvent) => {
     console.log('canvasRect.x: ', canvasRect.x);
     console.log('canvasRect.y: ', canvasRect.y);
 
-
-
-    const snapResult = getSnappedPosition(item.id, targetX, targetY)
+    const targetElement = e.target as HTMLElement;
+    const idValue = targetElement.id;
+    const item = droppedComponents.find((i) => i.id === idValue)!
+    if (!item) return
+    const snapResult = getSnappedPosition(item.id, item.x, item.y)
 
     item.x = snapResult.x
     item.y = snapResult.y
 
+    console.log('snapResult:',snapResult);
+    
+
     guidelines.value = snapResult.guidelines
+    if (snapResult.guidelines.length > 0) {
+        debugger
+    }
 
     droppedComponents[0].x = e.clientX - canvasRect.x - 20
 
@@ -129,22 +149,29 @@ const lineStyle = (line: GuideLine) => {
     }
 }
 
-const snapThreshold = 5 // px
+const snapThreshold = 1 // px
 
 function getSnappedPosition(id: string, x: number, y: number) {
+
     const movingItem = droppedComponents.find((i) => i.id === id)!
     const guidelines: GuideLine[] = []
+
     let snapX = x
     let snapY = y
 
     // 拖拽中的边界线（左、中、右 / 上、中、下）
-    const movingEdges: { [key: string]: number } = {
+    const movingChartEdges: { [key: string]: number } = {
+
+        // 左、中、右
         left: x,
-        right: x + movingItem.width,
         centerX: x + movingItem.width / 2,
+        right: x + movingItem.width,
+
+        // 上、中、下
         top: y,
-        bottom: y + movingItem.height,
-        centerY: y + movingItem.height / 2
+        centerY: y + movingItem.height / 2,
+        bottom: y + movingItem.height
+
     }
 
 
@@ -153,28 +180,36 @@ function getSnappedPosition(id: string, x: number, y: number) {
 
     // 遍历其他元素做对比
     for (const other of droppedComponents) {
+
         if (other.id === id) continue
 
-        const otherEdges: { [key: string]: number } = {
+        const otherChartEdges: { [key: string]: number } = {
+
+            // 左、中、右
             left: other.x,
-            right: other.x + other.width,
             centerX: other.x + other.width / 2,
+            right: other.x + other.width,
+
+            // 上、中、下
             top: other.y,
-            bottom: other.y + other.height,
-            centerY: other.y + other.height / 2
+            centerY: other.y + other.height / 2,
+            bottom: other.y + other.height
+
         }
-
-
 
         // X 方向对齐：left / centerX / right
         for (const edge of xKeys) {
-            const value = movingEdges[edge]
+
+            const xValue = movingChartEdges[edge]
+
             for (const otherEdge of xKeys) {
-                const otherValue = otherEdges[otherEdge]
-                const diff = Math.abs(value - otherValue)
+
+                const otherValue = otherChartEdges[otherEdge]
+                const diff = Math.abs(xValue - otherValue)
+
                 if (diff <= snapThreshold) {
                     guidelines.push({
-                        id: `v-${otherValue}`,
+                        id: `v-${u.uuid()}`,
                         direction: 'vertical',
                         position: otherValue
                     })
@@ -182,20 +217,20 @@ function getSnappedPosition(id: string, x: number, y: number) {
                     if (edge === 'left') snapX = otherValue
                     if (edge === 'centerX') snapX = otherValue - movingItem.width / 2
                     if (edge === 'right') snapX = otherValue - movingItem.width
-                    
+
                 }
             }
         }
 
         // Y 方向对齐：top / centerY / bottom
         for (const edge of yKeys) {
-            const value = movingEdges[edge]
+            const value = movingChartEdges[edge]
             for (const otherEdge of yKeys) {
-                const otherValue = otherEdges[otherEdge]
+                const otherValue = otherChartEdges[otherEdge]
                 const diff = Math.abs(value - otherValue)
                 if (diff <= snapThreshold) {
                     guidelines.push({
-                        id: `h-${otherValue}`,
+                        id: `h-${u.uuid()}`,
                         direction: 'horizontal',
                         position: otherValue
                     })
@@ -227,8 +262,24 @@ function getSnappedPosition(id: string, x: number, y: number) {
         position: absolute;
         background: #fff;
         border: 1px solid #000;
-        padding: 4px 8px;
+        // padding: 4px 8px;
         user-select: none;
+        display: inline-flex;
+        background: red;
+    }
+
+    .guide-line {
+        position: absolute;
+        background: red;
+        z-index: 1000;
+
+        &.vertical {
+            width: 1px;
+        }
+
+        &.horizontal {
+            height: 1px;
+        }
     }
 }
 </style>
