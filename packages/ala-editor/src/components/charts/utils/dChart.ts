9cb56245bf68cd39efd245a2a404b3ce62dec397
darcy.zhang <tech.darcy.zhang@outlook.com>
2025-06-04 18:44:26 +0800

    feat(折线图): 完善 折线图 动画效果

--- packages/ala-editor/src/components/charts/utils/dChart.ts
+++ packages/ala-editor/src/components/charts/utils/dChart.ts
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2025-05-26 13:44:21
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2025-06-04 18:32:38
+ * @LastEditTime: 2025-06-04 18:40:14
  * @FilePath: /1-low-coding/packages/ala-editor/src/components/charts/utils/dChart.ts
  * @Description: 
  * 
