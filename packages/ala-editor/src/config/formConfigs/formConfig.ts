a0ab5c7aa636548af5a1fc8d17c474cd5467b867
darcy.zhang <tech.darcy.zhang@outlook.com>
2025-06-08 11:28:53 +0800

    refactor(删除冗余代码): 删除冗余代码

--- packages/ala-editor/src/config/formConfigs/formConfig.ts
+++ packages/ala-editor/src/config/formConfigs/formConfig.ts
@@ -4,7 +4,7 @@ import { AlaField } from "@/config/fieldSchemas"
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2025-06-08 10:49:33
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2025-06-08 11:23:30
+ * @LastEditTime: 2025-06-08 11:28:19
  * @FilePath: /1-low-coding/packages/ala-editor/src/config/formConfigs/formConfig.ts
  * @Description: 
  * 
@@ -15,6 +15,6 @@ export default interface FormConfig {
     detailAttr: { [key: string]: any }
     formFields: Array<AlaField>
     detailFields: Array<any>
-    pageApi: String
+    pageApi: string
     pageFields?: Array<any>
 }
