11c6025011c9dd24c40bfedf3524641457a104ec
darcy.zhang <tech.darcy.zhang@outlook.com>
2025-11-21 15:49:09 +0800

    refactor(删除冗余代码): 删除冗余代码

--- packages/ala-editor/src/components/cps/dynamic/DetailFileColumn.vue
+++ packages/ala-editor/src/components/cps/dynamic/DetailFileColumn.vue
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-12-25 16:15:21
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2025-11-21 15:42:40
+ * @LastEditTime: 2025-11-21 15:48:52
  * @FilePath: /1-low-coding/packages/ala-editor/src/components/cps/dynamic/DetailFileColumn.vue
  * @Description: 
  * 
@@ -126,7 +126,6 @@ defineEmits(['refresh'])
 </script>
 
 <style scoped lang="scss">
-.title {}
 
 .value {
     .files {
@@ -147,6 +146,5 @@ defineEmits(['refresh'])
         color: #aaa;
     }
 
-    div {}
 }
 </style>
