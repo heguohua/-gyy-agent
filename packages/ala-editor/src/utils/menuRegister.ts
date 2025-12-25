35df0d0af6b55d4d35b4c45b7d5551fb7f830cdf
darcy.zhang <tech.darcy.zhang@outlook.com>
2025-12-25 08:51:54 +0800

    add 添加AI告警相关模块

--- packages/ala-editor/src/utils/menuRegister.ts
+++ packages/ala-editor/src/utils/menuRegister.ts
@@ -2,7 +2,7 @@
  * @Author: darcy.zhang , tech.darcy.zhang@outlook.com
  * @Date: 2024-11-10 12:57:08
  * @LastEditors: darcy.zhang , tech.darcy.zhang@outlook.com
- * @LastEditTime: 2025-12-24 21:41:04
+ * @LastEditTime: 2025-12-25 08:50:37
  * @FilePath: /1-low-coding/packages/ala-editor/src/utils/menuRegister.ts
  * @Description: 
  * 
@@ -211,7 +211,12 @@ export const allMenuComponents: MenuComponent = {
     },
     "/camera/videos": {
         name: "cameraVideosPage",
-        component: "../pages/iot/camera/index-card.vue",
+        component: "../pages/iot/camera/video-card.vue",
+        requiresAuth: true,
+    },
+    "/camera/aiAlarmDashboard": {
+        name: "aiAlarmDashboardPage",
+        component: "../pages/iot/camera/alarm-card.vue",
         requiresAuth: true,
     },
     "/physical/servers": {
